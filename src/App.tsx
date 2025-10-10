b4724dda9a71aeddbc29613001140d8faec33878
Matías Presuttari <matiasp@globalgestion.com.ar>
2025-10-10 15:16:30 -0300

    Modulo register creado

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
-
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 import './App.css';
 import MainLayout from './layouts/MainLayout';
+import { Register } from './pages/register';
 
 function App() {
     return (
-        <MainLayout>
-            {/* Mantén aquí el contenido actual o futuras rutas */}
-        </MainLayout>
+        <Router>
+            <MainLayout>
+                <Routes>
+                    <Route path="/" element={<div>Home Page</div>} />
+                    <Route path="/register" element={<Register/>} />
+                </Routes>
+            </MainLayout>
+        </Router>
     );
 }
 
